de91dab0b6abc57618fdea4a7becf116a9b8347b
asnet <asnet@asnets-MacBook-Pro.local>
2021-02-22 00:05:27 +0200

    Code for review

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,21 +11,7 @@
 
                 {{--///////////////////--}}
 
-                Password: <input type="password" value="FakePSW" id="myInput">
 
-                <!-- An element to toggle between password visibility -->
-                <input type="checkbox" onclick="myFunction()">Show Password
-
-                <script>
-                    function myFunction() {
-                        var x = document.getElementById("myInput");
-                        if (x.type === "password") {
-                            x.type = "text";
-                        } else {
-                            x.type = "password";
-                        }
-                    }
-                </script>
 
                 @foreach($items as $item)
                     <div class="bg-white shadow-xl rounded-lg overflow-hidden">
